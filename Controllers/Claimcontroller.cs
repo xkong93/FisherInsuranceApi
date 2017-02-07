fbe71c94ba252eeb9280e37de7eb0a62ed7a6c42
xkong93 <k.xuan29@gmail.com>
2017-02-06 20:29:30 -0500

    2/6

---
+++ Controllers/Claimcontroller.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FisherInsuranceApi.Controllers {
+
+
+[Route("api/customercare/claims")]
+public class ClaimsController : Controller
+{
+// POST api/customercare/claims
+        
+        [HttpPost] 
+        public IActionResult Post([FromBody]string value) 
+        { 
+            return Created("", value); 
+        }
+
+// GET api/customercare/claims/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id) {
+            return Ok("The id is: " + id);
+        }
+
+// PUT api/customercare/claims/id
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]string value) {
+            return NoContent();
+        }
+
+// DELETE api/customercare/claims/id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) {
+            return Delete(id);
+        }
+
+}}
